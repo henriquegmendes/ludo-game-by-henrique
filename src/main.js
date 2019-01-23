1c6aa6e03b484bf162fca2d0416f5686c9ce9a3d
Henrique Mendes <henrique.gmendes@hotmail.com>
2019-01-23 02:11:11 -0200

    Second commit. Blue Pawn 1 moving until the finish case, button effects and message effects already working

--- src/main.js
+++ src/main.js
@@ -1,7 +1,91 @@
 const startButton = document.getElementById('button');
+const bluePlayButton = document.getElementById('blue-play');
+const yellowPlayButton = document.getElementById('yellow-play');
+const greenPlayButton = document.getElementById('green-play');
+const redPlayButton = document.getElementById('red-play');
+const startMessage = document.getElementById('start-message');
+const blueMessage = document.getElementById('blue-message');
+const yellowMessage = document.getElementById('yellow-message');
+const greenMessage = document.getElementById('green-message');
+const redMessage = document.getElementById('red-message');
+const blue1 = document.getElementById('b1');
+const blue2 = document.getElementById('b2');
+const blue3 = document.getElementById('b3');
+const blue4 = document.getElementById('b4');
+const yellow1 = document.getElementById('y1');
+const yellow2 = document.getElementById('y2');
+const yellow3 = document.getElementById('y3');
+const yellow4 = document.getElementById('y4');
+const green1 = document.getElementById('g1');
+const green2 = document.getElementById('g2');
+const green3 = document.getElementById('g3');
+const green4 = document.getElementById('g4');
+const red1 = document.getElementById('r1');
+const red2 = document.getElementById('r2');
+const red3 = document.getElementById('r3');
+const red4 = document.getElementById('r4');
 
 startButton.addEventListener('click', () => {
-  buttonDice.style.display = 'block';
+  startButton.style.display = 'none';
+  bluePlayButton.style.display = 'block';
+  yellowPlayButton.style.display = 'none';
+  greenPlayButton.style.display = 'none';
+  redPlayButton.style.display = 'none';
+  startMessage.style.display = 'block';
+  for (i = 1; i <= 4; i += 1) {
+    document.getElementById('b' + i).style.display = 'block';
+    document.getElementById('y' + i).style.display = 'block';
+    document.getElementById('g' + i).style.display = 'block';
+    document.getElementById('r' + i).style.display = 'block';
+  }
+});
+
+bluePlayButton.addEventListener('click', () => {
   rollDice();
-  firstMovementBlue();
+});
+
+yellowPlayButton.addEventListener('click', () => {
+  rollDice();
+});
+
+greenPlayButton.addEventListener('click', () => {
+  rollDice();
+});
+
+redPlayButton.addEventListener('click', () => {
+  rollDice();
+});
+
+blue1.addEventListener('click', () => {
+  if (document.getElementById('board-blue-base-1').children.length === 0) {
+    moveBlue1();
+  }
+  if (document.getElementById('board-blue-base-1').children.length > 0 && diceResult === 1 || document.getElementById('board-blue-base-1').children.length > 0 && diceResult === 6) {
+    document.getElementById(newPosition).appendChild(document.getElementById('b1'));
+  }
+  bluePlayButton.style.display = 'none';
+  yellowPlayButton.style.display = 'block';
+  startMessage.style.display = 'none';
+  yellowMessage.style.display = 'block';
+});
+
+yellow1.addEventListener('click', () => {
+  yellowPlayButton.style.display = 'none';
+  greenPlayButton.style.display = 'block';
+  yellowMessage.style.display = 'none';
+  greenMessage.style.display = 'block';
+});
+
+green1.addEventListener('click', () => {
+  greenPlayButton.style.display = 'none';
+  redPlayButton.style.display = 'block';
+  greenMessage.style.display = 'none';
+  redMessage.style.display = 'block';
+});
+
+red1.addEventListener('click', () => {
+  redPlayButton.style.display = 'none';
+  bluePlayButton.style.display = 'block';
+  redMessage.style.display = 'none';
+  blueMessage.style.display = 'block';
 });
--- src/pawns.js
+++ src/pawns.js
@@ -6,88 +6,137 @@
 // Pawn.prototype.advance = function () {
 //   this.position = this.position + diceResult;
 // };
-const blue1 = document.getElementById('b1');
-const blue2 = document.getElementById('b2');
-const blue3 = document.getElementById('b3');
-const blue4 = document.getElementById('b4');
-const yellow1 = document.getElementById('y1');
-const yellow2 = document.getElementById('y2');
-const yellow3 = document.getElementById('y3');
-const yellow4 = document.getElementById('y4');
-const green1 = document.getElementById('g1');
-const green2 = document.getElementById('g2');
-const green3 = document.getElementById('g3');
-const green4 = document.getElementById('g4');
-const red1 = document.getElementById('r1');
-const red2 = document.getElementById('r2');
-const red3 = document.getElementById('r3');
-const red4 = document.getElementById('r4');
-
 const setup = {
   colors: ['blue', 'yellow', 'green', 'red'],
   initialPosition: [21, 4, 55, 38],
-  finalPosition: [17, 68, 51, 34]
+  finalPosition: [17, 68, 51, 34],
+  finishLine: [25, 76, 59, 42]
 };
+let initialPosition = setup.initialPosition[0];
+let newPosition = 'board-pos-' + initialPosition;
+let click = 0;
 
-function firstMovementBlue () {
-  const allBluePawns = Array.from(document.getElementsByClassName('blue-pawn'));
-  for (let i = 1; i <= allBluePawns.length; i += 1) {
-    allBluePawns[i - 1].addEventListener('click', (event) => {
-      if (document.getElementById('board-blue-base-1').children.length > 0 && document.getElementById('board-blue-base-2').children.length > 0 && document.getElementById('board-blue-base-3').children.length > 0 && document.getElementById('board-blue-base-4').children.length > 0) {
-        if (diceResult === 1 || diceResult === 6) {
-          let newB = 'b' + i;
-          let newBoardStartPos = 'board-pos-' + setup.initialPosition[0];
-          document.getElementById(newBoardStartPos).appendChild (document.getElementById(newB));
-        }
-      }
-    });
+function blueResetBoardCount() {
+  if (initialPosition === 69) {
+    initialPosition = 1;
+    newPosition = 'board-pos-' + initialPosition;
+  } else if (initialPosition === 70) {
+    initialPosition = 2;
+    newPosition = 'board-pos-' + initialPosition;
+  } else if (initialPosition === 71) {
+    initialPosition = 3;
+    newPosition = 'board-pos-' + initialPosition;
+  } else if (initialPosition === 72) {
+    initialPosition = 4;
+    newPosition = 'board-pos-' + initialPosition;
+  } else if (initialPosition === 73) {
+    initialPosition = 5;
+    newPosition = 'board-pos-' + initialPosition;
+  } else if (initialPosition === 74) {
+    initialPosition = 6;
+    newPosition = 'board-pos-' + initialPosition;
   }
 }
 
-// function bluePawnMovement() {
+function blueFinalPath() {
+  if (click > 10 && initialPosition === 18) {
+    newPosition = 'board-blue-' + initialPosition;
+  } else if (click > 10 && initialPosition === 19) {
+    newPosition = 'board-blue-' + initialPosition;
+  } else if (click > 10 && initialPosition === 20) {
+    newPosition = 'board-blue-' + initialPosition;
+  } else if (click > 10 && initialPosition === 21) {
+    newPosition = 'board-blue-' + initialPosition;
+  } else if (click > 10 && initialPosition === 22) {
+    newPosition = 'board-blue-' + initialPosition;
+  } else if (click > 10 && initialPosition === 23) {
+    newPosition = 'board-blue-' + initialPosition;
+  } else if (click > 10 && initialPosition === 24) {
+    newPosition = 'board-blue-' + initialPosition;
+  }
+}
+
+function blue1Finish() {
+  if (newPosition.search('blue') !== -1) {
+    if (diceResult === 6) {
+      document.getElementById('board-blue-25').appendChild(document.getElementById('b1'));
+    }
+    return;
+  }
+}
+
+function moveBlue1() {
+  blue1Finish();
+  click += 1;
+  initialPosition += diceResult;
+  newPosition = 'board-pos-' + initialPosition;
+  blueResetBoardCount();
+  blueFinalPath();
+  document.getElementById(newPosition).appendChild(document.getElementById('b1'));
+  diceResult = 0;
+}
+
+// function firstMovementBlue () {
 //   const allBluePawns = Array.from(document.getElementsByClassName('blue-pawn'));
-//   for (let i = 0; i < allBluePawns.length; i += 1) {
+//   for (let i = 0; i <= allBluePawns.length - 1; i += 1) {
 //     allBluePawns[i].addEventListener('click', (event) => {
-//       // function moving () {
-        
-//       // }
-//       console.log('blue', event.target);
+//       if (document.getElementById('board-blue-base-1').children.length > 0 && document.getElementById('board-blue-base-2').children.length > 0 && document.getElementById('board-blue-base-3').children.length > 0 && document.getElementById('board-blue-base-4').children.length > 0) {
+//         diceValue.push(diceResult);
+//         if (diceValue[0] === 1 || diceValue[0] === 6) {
+//           let newB = 'b' + (i + 1);
+//           const newBoardStartPos = 'board-pos-' + setup.initialPosition[0];
+//           document.getElementById(newBoardStartPos).appendChild (document.getElementById(newB));
+//         }
+//       }
 //     });
 //   }
 // }
 
-// function yellowPawnMovement() {
-//   const allYellowPawns = Array.from(document.getElementsByClassName('yellow-pawn'));
-//   for (let i = 0; i < allYellowPawns.length; i += 1) {
-//     allYellowPawns[i].addEventListener('click', (event) => {
-//       // function moving () {
-        
-//       // }
-//       console.log('red', event.target);
-//     });
+// function movementBlue () {
+//   const allBluePawns = Array.from(document.getElementsByClassName('blue-pawn'));
+//   for (let i = 0; i <= allBluePawns.length - 1; i += 1) {
+//     allBluePawns[i].addEventListener('click', (event) => {
+//       if (document.getElementById('board-pos-21').children.length > 0) {
+//         diceValue.push(diceResult);
+//       let newB = 'b' + (i + 1);
+//       const newPosition = setup.initialPosition[0] + diceResult;
+//       const newBoardPos = 'board-pos-' + newPosition;
+//       document.getElementById(newBoardPos).appendChild (document.getElementById(newB));
+//       }
+//     })
 //   }
 // }
 
-// function greenPawnMovement() {
-//   const allGreenPawns = Array.from(document.getElementsByClassName('green-pawn'));
-//   for (let i = 0; i < allGreenPawns.length; i += 1) {
-//     allGreenPawns[i].addEventListener('click', (event) => {
-//       // function moving () {
-        
-//       // }
-//       console.log('red', event.target);
-//     });
-//   }
+// if(document.getElementById('board-blue-7').children.length === 0) {
+//   let j = 1;
+//   j += 1
+//   diceValue.push(diceResult);
+//   let newB = 'b' + (i + 1);
+//   let newPos = 0;
+//   newPos = newPos + setup.initialPosition[0] + diceValue[j - 1];
+//   let newBoardPos = 'board-pos-' + newPos;
+//   document.getElementById(newBoardPos).appendChild (document.getElementById(newB));
 // }
 
-// function redPawnMovement() {
-//   const allRedPawns = Array.from(document.getElementsByClassName('Red-pawn'));
-//   for (let i = 0; i < allRedPawns.length; i += 1) {
-//     allRedPawns[i].addEventListener('click', (event) => {
-//       // function moving () {
-        
-//       // }
-//       console.log('red', event.target);
-//     });
-//   }
+
+
+// if(document.getElementById(newBoardPos).children.length > 0) {
+//   newPosition += diceResult;
+//   newBoardPos = 'board-pos-' + newPosition;
+//   document.getElementById(newBoardPos).appendChild (document.getElementById(newB));
+// }
+
+// function firstMovementBlue1 () {
+//   const allBluePawns = document.getElementsByClassName('blue-pawn');
+//   console.log(allBluePawns);
+//   allBluePawns.addEventListener('click', (event) => {
+//     console.log(event.currentTarget);
+//     if (document.getElementById('board-blue-base-1').children.length > 0 && document.getElementById('board-blue-base-2').children.length > 0 && document.getElementById('board-blue-base-3').children.length > 0 && document.getElementById('board-blue-base-4').children.length > 0) {
+//       if (diceResult === 1 || diceResult === 6) {
+//         let newB = 'b' + i;
+//         const newBoardStartPos = 'board-pos-' + setup.initialPosition[0];
+//         document.getElementById(newBoardStartPos).appendChild(document.getElementById('b1'));
+//       }
+//     }
+//   });
 // }
